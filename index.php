839d6736267e20577ab03c30047ab1005b6de64e
Vo Thi Anh Thu <91442570+VoThiAnhThu@users.noreply.github.com>
2023-10-17 16:34:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -42,105 +42,60 @@
 
 
         <section class="content">
-            <h3 class="content-title">TIỂU THUYẾT MỚI NHẤT</h3>
-            <div class="row row-cols-1 row-cols-md-3 g-4">
+            <h3 class="content-title">ẤN PHẨM MỚI NHẤT</h3>
+            <div class="row">
                 <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
+                    <div class="index-card card">
+                        <img src="/ct258-tmdt/img/sanpham/20231003_163642_DeMen_phu2.jpg" class="index-card-img card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Dế Mèn Phiêu Lưu Kí</h5>
+                            <p class="card-text">Tác phẩm tiêu biểu của nhà văn Tô Hoài</p>
+                            <p><del>120000</del></p>
+                            <p>95000</p>
+                            <a href="chitietsanpham.php?sp_ma=3" class="btn btn-primary">Chi tiết sản phẩm</a>
+                        </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="content">
-            <h3 class="content-title">TẠP CHÍ VỪA RA MẮT</h3>
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
+                    <div class="index-card card">
+                        <img src="/ct258-tmdt/img/sanpham/20231003_163606_Elle_Jennie.jpg" class="index-card-img card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Tạp chí ELLE tháng 9</h5>
+                            <p class="card-text">Ấn bản mới nhất của tòa báo nước Pháp</p>
+                            <p><del>185000</del></p>
+                            <p>161000</p>
+                            <a href="chitietsanpham.php?sp_ma=10" class="btn btn-primary">Chi tiết sản phẩm</a>
+                        </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
+                    <div class="index-card card">
+                        <img src="/ct258-tmdt/img/sanpham/20231003_171341_Anh_em_nhà_Karamazov.jpg" class="index-card-img card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Anh em nhà Karamazov</h5>
+                            <p class="card-text">Đời người ít nhất đọc 1 lần trong đời</p>
+                            <p><del>145000</del></p>
+                            <p>80000</p>
+                            <a href="chitietsanpham.php?sp_ma=5" class="btn btn-primary">Chi tiết sản phẩm</a>
+                        </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
+                    <div class="index-card card">
+                        <img src="/ct258-tmdt/img/sanpham/20231003_171357_ho-so-bi-an.jpg" class="index-card-img card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Hồ Sơ Bí Ẩn</h5>
+                            <p class="card-text">Khám phá chân tướng các vụ án trong quá khứ</p>
+                            <p><del>196000</del></p>
+                            <p>178000</p>
+                            <a href="chitietsanpham.php?sp_ma=7" class="btn btn-primary">Chi tiết sản phẩm</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
 
-
-        <section class="content">
-            <h3 class="content-title">TRUYỆN NGẮN MỚI PHÁT HÀNH</h3>
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
+       
     </div>
     <!--Start Footer-->
     <?php
