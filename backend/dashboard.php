a6e101b1e963e3f73e3edc86556b426b1a0da250
Vo Thi Anh Thu <91442570+VoThiAnhThu@users.noreply.github.com>
2023-11-11 00:09:45 +0700

    Update dashboard.php 11/10

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -5,11 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        div {
-            border: 1px solid black;
-        }
-    </style>
+
 
     <?php
     include_once __DIR__ . '/layouts/style.php';
@@ -21,21 +17,45 @@
     ?>
 
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-3">
-        <?php
-        include_once __DIR__ . '/layouts/sidebar.php';
-        ?>
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">DANH MỤC SẢN PhẨM</h5>
+                            <p class="card-text">Quản lý các mục thêm sửa xóa sản phẩm</p>
+                            <a href="/ct258-tmdt/backend/sanpham/index.php" class="btn btn-primary">Xem chi tiết</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">DANH MỤC HÌNH SẢN PHẨM</h5>
+                            <p class="card-text">Quản lý các mục thêm sửa xóa hình ảnh sản phẩm</p>
+                            <a href="/ct258-tmdt/backend/hinhsanpham/index.php" class="btn btn-primary">Xem chi tiết</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">DANH MỤC KHÁCH HÀNG</h5>
+                            <p class="card-text">Quản lý thông tin khách hàng đã đăng kí tài khoản</p>
+                            <a href="//ct258-tmdt/backend/khachhang/index.php"" class="btn btn-primary">Xem chi tiết</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">DANH MỤC ĐƠN HÀNG</h5>
+                            <p class="card-text">Quản lý thông tin khách hàng đã đặt mua sản phẩm</p>
+                            <a href="/ct258-tmdt/backend/donhang/index.php"" class="btn btn-primary">Xem chi tiết</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-9">
-            NOI DUNG CUA TRANG WEB
-            
-            <br><br><br><br><br>
-            
-            <div id="editor"></div>
-
-            </div>
-        </div>
+    </div>
 
     <?php
     include_once __DIR__ . '/layouts/footer.php';
@@ -45,13 +65,7 @@
     include_once __DIR__ . '/layouts/script.php';
     ?>
 
-    <script>
-        ClassicEditor
-            .create( document.querySelector( '#editor' ) )
-            .catch( error => {
-                console.error( error );
-            } );
-    </script>
+
 
 </body>
 </html>
