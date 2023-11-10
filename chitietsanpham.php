203de7bf08f828922628d93aad8672ee94140120
Vo Thi Anh Thu <91442570+VoThiAnhThu@users.noreply.github.com>
2023-11-10 23:43:31 +0700

    Update chitietsanpham.php 11/10 còn thiếu

--- chitietsanpham.php
+++ chitietsanpham.php
@@ -20,6 +20,9 @@
     $sp_ma = $_GET['sp_ma'];
     $sqlSanPham = "SELECT * FROM sanpham
                     WHERE sp_ma = $sp_ma;";
+    
+
+    
     //3. Thực thi
     $resultSanPham = mysqli_query($conn, $sqlSanPham);
     //4. Phân tách
@@ -39,6 +42,36 @@
             'sp_ma' => $row['sp_ma'],
         );
     }
+    //Lấy các sản phẩm liên quan
+    $resultSanPham = mysqli_query($conn, $sqlSanPham);
+    if ($resultSanPham) {
+        $rowSanPham = mysqli_fetch_assoc($resultSanPham);
+        $lsp_ma = $rowSanPham['lsp_ma'];
+
+        $sqlLoaiSanPham = "SELECT * FROM sanpham WHERE lsp_ma = $lsp_ma AND sp_ma <> $sp_ma";
+        $resultLoaiSanPham = mysqli_query($conn, $sqlLoaiSanPham);
+        $dataLoaiSanPham = [];
+        while ($rowLoaiSanPham = mysqli_fetch_assoc($resultLoaiSanPham)) {
+            $dataLoaiSanPham[] = array(
+                'sp_ma' => $rowLoaiSanPham['sp_ma'],
+                'sp_ten' => $rowLoaiSanPham['sp_ten'],
+                'sp_gia' => $rowLoaiSanPham['sp_gia'],
+                'sp_giacu' => $rowLoaiSanPham['sp_giacu'],
+                'sp_mota_ngan' => $rowLoaiSanPham['sp_mota_ngan'],
+                'sp_ngaycapnhat' => $rowLoaiSanPham['sp_ngaycapnhat'],
+                'sp_soluong' => $rowLoaiSanPham['sp_soluong'],
+                'lsp_ma' => $rowLoaiSanPham['lsp_ma'],
+                'nsx_ma' => $rowLoaiSanPham['nsx_ma'],
+            );
+        }
+    } else {
+        echo "Lỗi truy vấn: " . mysqli_error($conn);
+    }
+
+
+
+    //var_dump($dataLoaiSanPham);
+
     //var_dump($dataSanPham);
     //var_dump($dataHinhSanPham);
     ?>
@@ -78,8 +111,8 @@
                     <?php endif; ?>
                     <h1><?= $dataSanPham['sp_ten'] ?></h1>
                     <div class="sp-drs">
-                        <b>Mô tả ngắn:</b>
-                        <?= $dataSanPham['sp_mota_ngan'] ?>
+                        <b>Mô tả ngắn:</b><br>
+                        <p><?= $dataSanPham['sp_mota_ngan'] ?></p>
                     </div>
                         <?php if($dataSanPham['sp_giacu'] != $dataSanPham['sp_gia']):  ?>
                             <del><span class="text-muted" style="font-size: 0.8rem;">
@@ -94,9 +127,46 @@
                         </div>
                         
                     <button name="btnDatMua"  class="nut-datmua">Chọn mua</button>
+                    <a href="sanpham.php" class="btn btn-primary">Quay về</a>
                 </form>
             </div>
-        </div>    
+        </div>
+        <!--Sản phẩm gợi ý-->
+        <div class="container">
+            <h3 style="text-align: left">Các Sản Phẩm Liên Quan</h3>
+            <div class="row">
+                <?php foreach($dataLoaiSanPham as $lsp): ?>
+                    <div class="col-md-3">
+                        <div class="sp-item card">
+                            <?php if(!empty($sp['hsp_tentaptin'])): ?>
+                                <img  src="/ct258-tmdt/assets/uploads/<?=$sp['hsp_tentaptin']?>" class="card-img-top" alt="...">
+                            <?php else: ?>
+                                <img  src="/ct258-tmdt/assets/img/default-image.jpg" class="card-img-top" alt="...">
+                            <?php endif; ?>
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $lsp['sp_ten'] ?></h5>
+                                <p class="card-text">
+                                    <?= $lsp['sp_mota_ngan'] ?>    
+                                </p>
+                                <p>
+                                    <?php if($lsp['sp_giacu'] != $lsp['sp_gia']): ?>
+                                        <del>
+                                            <span class="text-muted">
+                                                <?=number_format( $lsp['sp_giacu'],0,'.',',') ?>
+                                            </span>
+                                        </del>
+                                    <?php endif; ?>
+                                    <br>
+                                    <b><?=number_format( $lsp['sp_gia'],0,'.',',') ?></b>
+                                </p>
+                                <a href="chitietsanpham.php?sp_ma=<?=$lsp['sp_ma'] ?>" class="btn btn-primary">Chi tiết sản phẩm</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+            
     </div>
     <!--Start Footer-->
     <?php
