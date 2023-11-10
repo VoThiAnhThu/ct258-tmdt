ad1b23a45e8a7dae927fe865c494290ade0c172b
Vo Thi Anh Thu <91442570+VoThiAnhThu@users.noreply.github.com>
2023-11-10 23:44:30 +0700

    Update style.php

--- layouts/style.php
+++ layouts/style.php
@@ -2,8 +2,10 @@
 <link rel="stylesheet"  href="/ct258-tmdt/assets/vendors/datatables/datatables.min.css">
 <link rel="stylesheet" href="/ct258-tmdt/assets/vendors/fontawesome-free-6.3.0-web/css/all.min.css">
 <link rel="stylesheet" href="/ct258-tmdt/css/chung.css">
-<link rel="stylesheet" href="/ct258-tmdt/css/home1.css">
+<link rel="stylesheet" href="/ct258-tmdt/css/index.css">
+<link rel="stylesheet" href="/ct258-tmdt/css/home3.css">
 <link rel="stylesheet" href="/ct258-tmdt/css/sanpham2.css">
+<link rel="stylesheet" href="/ct258-tmdt/css/loaisanpham1.css">
 <link rel="stylesheet" href="/ct258-tmdt/css/lienhe.css">
 <link rel="stylesheet" href="/ct258-tmdt/css/gioithieu.css">
 <link rel="stylesheet" href="/ct258-tmdt/css/dangnhap1.css">
