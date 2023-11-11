6e5d1691193244afe30333cff9b083b4ae6d0455
Vo Thi Anh Thu <91442570+VoThiAnhThu@users.noreply.github.com>
2023-11-11 18:49:58 +0700

    Update footer.php 11/11

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,9 +1,9 @@
-<footer class="footer">
+<footer class="footer" style="margin-top:30px;">
     <div class="footer-item">
         <div class="footer-title">THEO DÕI CHÚNG TÔI TẠI</div>
-        <a href=""><i class="footer-icon fa-brands fa-facebook"></i></a>
-        <a href=""><i class="footer-icon fa-brands fa-instagram"></i></a>
-        <a href=""><i class="footer-icon fa-brands fa-twitter"></i></i></a>
+        <a href="https://www.facebook.com/"><i class="footer-icon fa-brands fa-facebook"></i></a>
+        <a href="https://www.instagram.com/"><i class="footer-icon fa-brands fa-instagram"></i></a>
+        <a href="https://twitter.com/"><i class="footer-icon fa-brands fa-twitter"></i></i></a>
     </div>
     <div class="footer-item">
         <div class="footer-title">
